17e8b2f811903bf50a1bcfbb878522aa885ee118
Prashant Srivastava <powprashant@gmail.com>
2018-05-18 18:58:57 +0530

    Update foo.lua

--- src/foo.lua
+++ src/foo.lua
@@ -2,22 +2,21 @@
 
 function lua_fun(x,y)
     registerButonClick( "callback" , x)
-	registerButonClick( "callback" , 42)
+    registerButonClick( "callback" , 42)
     return mydoubler(x)+ mydoubler(y)
 end
 
 function lua_test( n )
     io.write( "Inside Lua test Function\n" );
-	print( n );
+    print( n );
 end
 
 function callback(widId)
    io.write( string.format("Inside Lua callback = %d\n", widId));
    
    if( widId ~= 42 ) then
-   print("Jack\n");
+      print("Jack\n");
    else
-   print("Queen\n");
+      print("Queen\n");
    end
-   
 end
